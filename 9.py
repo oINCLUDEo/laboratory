da08dd22d5b6cd6ef7280fca001d9e7d7b2ae040
oINCLUDEo <nikita-zhuklin@rambler.ru>
2023-10-02 18:23:34 +0400

    update files

--- 9.py
+++ 9.py
@@ -1,42 +1,48 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton, QMessageBox
-
-class RegistrationWindow(QWidget):
-    def __init__(self):
-        super().__init__()
-        layout = QVBoxLayout()
-        self.setWindowTitle("Окно регистрации")
-        self.setGeometry(100, 100, 400, 200)
-
-        self.label_username = QLabel("Введите логин:")
-        self.username_input = QLineEdit()
-        layout.addWidget(self.label_username)
-        layout.addWidget(self.username_input)
-
-        self.label_password = QLabel("Введите пароль:")
-        self.password_input = QLineEdit()
-        self.password_input.setEchoMode(QLineEdit.Password)
-        layout.addWidget(self.label_password)
-        layout.addWidget(self.password_input)
-
-        self.register_button = QPushButton("Зарегистрироваться")
-        self.register_button.clicked.connect(self.register)
-        layout.addWidget(self.register_button)
-
-        self.setLayout(layout)
-
-    def register(self):
-        username = self.username_input.text()
-        password = self.password_input.text()
-
-        QMessageBox.information(self, "Регистрация успешна", "Пользователь " + str(username) + " успешно "
-                                                                                               "зарегистрирован!")
-        self.username_input.clear()
-        self.password_input.clear()
+import tkinter as tk
 
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    window = RegistrationWindow()
-    window.show()
-    sys.exit(app.exec_())
+def show_success_window():
+    success_window = tk.Toplevel(root)
+    success_window.title("Регистрация успешна")
+    success_label = tk.Label(success_window, text="Вы успешно зарегистрированы!")
+    success_label.pack()
+    ok_button = tk.Button(success_window, text="Окей", command=close_all_windows)
+    x = (success_window.winfo_screenwidth() - success_window.winfo_reqwidth()) // 2 + 75
+    y = (success_window.winfo_screenheight() - success_window.winfo_reqheight()) // 2 + 50
+    success_window.geometry("+%d+%d" % (x, y))
+    ok_button.pack()
+
+
+def close_all_windows():
+    root.destroy()  # Закрыть главное окно
+    new_empty_window = tk.Tk()
+    new_empty_window.title("Пустое окно")
+    new_empty_window.geometry("400x200")
+    x = (new_empty_window.winfo_screenwidth() - new_empty_window.winfo_reqwidth()) // 2
+    y = (new_empty_window.winfo_screenheight() - new_empty_window.winfo_reqheight()) // 2
+    new_empty_window.geometry("+%d+%d" % (x, y))
+
+
+root = tk.Tk()
+root.title("Окно регистрации")
+root.geometry("300x200")
+x = (root.winfo_screenwidth() - root.winfo_reqwidth()) // 2
+y = (root.winfo_screenheight() - root.winfo_reqheight()) // 2
+root.geometry("+%d+%d" % (x, y))
+
+label_username = tk.Label(root, text="Имя пользователя:")
+label_username.pack()
+
+entry_username = tk.Entry(root)
+entry_username.pack()
+
+label_password = tk.Label(root, text="Пароль:")
+label_password.pack()
+
+entry_password = tk.Entry(root, show="*")
+entry_password.pack()
+
+submit_button = tk.Button(root, text="Зарегистрироваться", command=show_success_window)
+submit_button.pack()
+
+root.mainloop()
