27aacd705d735e52b98e8b2eb8e5b8bbe2fdd7ff
oINCLUDEo <nikita-zhuklin@rambler.ru>
2023-12-08 17:45:55 +0400

    update files

--- 12.py
+++ 12.py
@@ -1,11 +1,18 @@
+# 11. Вычислить сумму знакопеременного ряда (|х(3n)|)/(3n)!, где х-матрица ранга к (к и матрица задаются случайным
+# образом), n - номер слагаемого. Сумма считается вычисленной, если точность вычислений будет не меньше t знаков
+# после запятой. У алгоритма д.б. линейная сложность. Операция умножения –поэлементная. Знак первого слагаемого  +.
+
 import random as ran
 import numpy as np
 import decimal as de
 
+# region Создание матрицы
 k = ran.randint(1, 10)
 x = np.random.uniform(-1, 1, (k, k))
+# endregion
 
 t = input("Введите число t > 0: ")
+# region Проверка ввода t
 while True:
     if t.isdigit() == True and 1 < int(t) <= 100:
         t = int(t)
@@ -14,16 +21,19 @@ while True:
         t = input("t не является числом, введите его ещё раз: ")
     elif int(t) < 1 or int(t) > 100:
         t = input("Число t меньше единицы или больше ста, введите его ещё раз: ")
+# endregion
 
-n = 1
-n3_factotial = 3
-result = de.Decimal(0)
+n = 1  # Номер слагаемого
+n3_factotial = 3  # Факториал
+result = de.Decimal(0)  # Результат
 de.getcontext().prec = t + 60  # установка точности
-count = de.Decimal(0)
-while abs(result.as_tuple().exponent) < t:  # цикл while пока число знаков после запятой не будет больше или равно t
-    result += de.Decimal(np.linalg.det(x ** (3 * n))) * de.Decimal(1 / n3_factotial)  # вычисление слагаемого
-    n += 1
-    n3_factotial *= n
-
-print('Матрица X:\n', x)  # вывод матрицы
+# region Вычисление слагаемого до точности с t
+while abs(result.as_tuple().exponent) < t:
+    result += de.Decimal(np.linalg.det(x ** (3 * n))) * de.Decimal(1 / n3_factotial)  # вычисление слагаемого/результата
+    n += 1  # Номер слагаемого увеличился
+    n3_factotial *= n  # Обновление факториала
+# endregion
+# region Вывод
+print('Матрица X:\n', x)
 print(f"Ответ: {result:.{t}f}")
+# endregion
