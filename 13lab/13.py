4d289f0b5b42bae57f150c0d72cc480f0ccb7ea4
oINCLUDEo <nikita-zhuklin@rambler.ru>
2023-12-08 18:00:53 +0400

    update files

--- 13lab/13.py
+++ 13lab/13.py
@@ -1,9 +1,9 @@
+# 11.	Определить количество пассажиров на борту в возрастном интервале мода +- 10 позиций и сколько из них выжило
 import csv
 
 # Открываем файл titanic.csv
 with open('titanic.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)  # Читаем файл как словарь
-
     # Создаем пустые списки для хранения возрастов и выживших пассажиров
     ages = []
     survived = []
@@ -20,8 +20,8 @@ with open('titanic.csv', newline='') as csvfile:
 
 # Вычисляем моду и диапазон возрастов для поиска пассажиров
 age_mode = max(set(ages), key=ages.count)
-lower_limit = age_mode - 10
-upper_limit = age_mode + 10
+lower_limit = age_mode - 10  # Нижний диапазон
+upper_limit = age_mode + 10  # Верхний диапозон
 
 # Считаем количество пассажиров в указанном возрастном интервале и количество выживших среди них
 passengers_in_range = sum(1 for age in ages if lower_limit <= age <= upper_limit)
